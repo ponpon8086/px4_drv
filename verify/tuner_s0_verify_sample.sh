7c0072252281ddca319c3bcc8269d8b7b810c69c
ponpon8086 <ponpon8086@gmail.com>
2025-04-03 00:32:09 +0000

    チューナー動作確認用のサンプルスクリプトを追加
    checksignal,recpt1は別途ビルドが必要(インストールスクリプトで自動生成)

---
+++ verify/tuner_s0_verify_sample.sh
@@ -0,0 +1,5 @@
+# NHK BS1を10秒録画、s0.tsとして保存
+./recpt1 --device /dev/px4video0 101 10 s0.ts 
+
+# NHK BS1の信号レベルをチェック
+#./checksignal --device /dev/px4video0 101
---
+++ verify/tuner_t0_verify_sample.sh
@@ -0,0 +1,5 @@
+# NHK総合を10秒録画、t0.tsとして保存
+./recpt1 --device /dev/px4video2 27 10 t0.ts
+
+# NHK総合の信号レベルをチェック
+#./checksignal --device /dev/px4video2 27
